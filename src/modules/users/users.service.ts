0ab92365deb7e6206a33e45814f9f0f86cf2a60a
Your Name <nguyphuquy1@gmail.com>
2024-04-05 16:04:25 +0700

    feature/change-modules/users

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,7 +5,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
   create(createUserDto: CreateUserDto) {
-    return 'My name is Quy';
+    return 'My name is Phu Quy';
   }
 
   findAll() {
